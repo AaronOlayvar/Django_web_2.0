0333702d736f233d99a71f56ec0571305ebe149e
AaronOlayvar <trixolayvar@gmail.com>
2022-11-16 20:03:51 +0000

    working category views

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from store.models import Product
 
+def category_detail(request, slug):
+    return render(request, 'store/category_detail.html')
 # Create your views here.
 def frontpage(request):
     products = Product.objects.all()[0:6]
--- petnet/urls.py
+++ petnet/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from core.views import frontpage, about
 urlpatterns = [
-    path('', include('store.urls')),
     path('', frontpage, name='frontpage'),
     path('admin/', admin.site.urls),
     path('about/', about, name='about'),
+    path('', include('store.urls')),
 ]
