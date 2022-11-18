8f4689fe74582b0aaa0c735477b99355d81509fe
AaronOlayvar <trixolayvar@gmail.com>
2022-11-18 14:23:37 +0000

    search function working

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
@@ -12,3 +13,9 @@ def product_detail(request, category_slug, slug):
     product = get_object_or_404(Product, slug=slug)
 
     return render(request, 'store/product_detail.html', {'product': product})
+
+def search(request):
+    query = request.GET.get('query','')
+    products = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'store/search.html', {'query':query, 'products': products, })
