288f7e97e8afc1afa78c9d36afe0cbd19623aec0
AaronOlayvar <trixolayvar@gmail.com>
2022-11-20 18:32:07 +0000

    incr decr func

--- store/views.py
+++ store/views.py
@@ -32,6 +32,20 @@ def cart_view(request):
 
     return render(request, 'store/cart_view.html', {'cart': cart})
 
+def change_quantity(request, product_id):
+    action = request.GET.get('action', '')
+
+    if action:
+        quantity = 1
+
+        if action == 'decrease':
+            quantity= -1
+
+        cart = Cart(request)
+        cart.add(product_id, quantity, True)
+        
+        return redirect('cart_view')
+
 def remove_from_cart(request, product_id):
     cart = Cart(request)
     cart.remove(product_id)
